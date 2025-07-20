7f35d4252f0f3a07345d4d76d53ba4daea848784
AnantaDevraj <anantadevraj773@gmail.com>
2025-07-21 02:31:39 +0530

    Complete react-router

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {} from "react";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import { Footer } from "flowbite-react";
+
+function App() {
+  return (
+    <>
+      {/* <Header />
+      <Home />
+      <Footer /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/User/User.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function User (){
+
+    const {userid} = useParams();
+    return (
+         <div className='bg-gray-600 text-3xl text-white p-4 flex justify-center items-center h-24'>
+            User: {userid}
+        </div>
+    )
+}
+
+export default User;
